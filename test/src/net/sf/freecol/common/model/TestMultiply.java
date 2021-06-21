0715f3fc544843384ef9ff588d7ec4a76246517a
TomTSP <66974328+TomTSP@users.noreply.github.com>
2021-06-21 14:47:06 +0200

    And now?

--- test/src/net/sf/freecol/common/model/TestMultiply.java
+++ test/src/net/sf/freecol/common/model/TestMultiply.java
@@ -1,29 +1,15 @@
 package net.sf.freecol.common.model;
 
 import org.junit.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 import net.sf.freecol.common.model.Multiply;
 
 public class TestMultiply extends TestCase {
-  private Multiply multiplier;
-  
-  @BeforeEach
-  public void setUp() throws Excepton {
-    multiplier = new Multiply();
-  }
-  
-  @Test                                               
-    @DisplayName("Multiplication test")   
-    public void testMultiply() {
-        assertEquals(64, multiplier.multiply(8,8),      
-        "Multiplication should work");          
-    }
+  private Multiply multiplier = new Multiply();
 
-  @RepeatedTest(3)                                    
-  @DisplayName("Multiplication with zero test")
+  public void testMultiply() {
+      assertEquals(64, multiplier.multiply(8,8), "Multiplication should work");          
+  }
+
   public void testMultiplyWithZero() {
       assertEquals(0, multiplier.multiply(0,8), "Multiple with zero should be zero");
       assertEquals(0, multiplier.multiply(8,0), "Multiple with zero should be zero");
